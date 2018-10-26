df497d9203d3dfc430807cb7d3e9df715f9c4680
guoliang <guoliang0511@qq.com>
2018-10-26 23:03:53 +0800

    27. Remove Element

---
+++ 0027/main.cpp
@@ -0,0 +1,67 @@
+// 27. Remove Element
+// https://leetcode.com/problems/remove-element/
+/*
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+Given nums = [3,2,2,3], val = 3,
+Your function should return length = 2, with the first two elements of nums being 2.
+It doesn't matter what you leave beyond the returned length.
+
+Example 2:
+Given nums = [0,1,2,2,3,0,4,2], val = 2,
+Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+
+Note that the order of those five elements can be arbitrary.
+
+It doesn't matter what values are set beyond the returned length.
+
+Clarification:
+Confused why the returned value is an integer but your answer is an array?
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeElement(nums, val);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int> &nums, int val) {
+        int count = 0;//等于val的元素的数量
+        int j = nums.size() - 1;//nums[j+1,nums.size-1]==val
+        for (int i = 0; i <= j;) {
+            if (nums[i] == val) {
+                swap(nums[i], nums[j]);
+                --j;
+                ++count;
+            } else {
+                ++i;
+            }
+        }
+        return nums.size() - count;
+    }
+};
+
+int main() {
+    vector<int> nums = {0, 1, 2, 2, 3, 0, 4, 2};
+    cout << Solution().removeElement(nums, 2) << endl;
+    for (const int &num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
+// Runtime: 4 ms, faster than 98.09% of C++ online submissions for Remove Element.
